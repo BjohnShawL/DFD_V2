acac9ea0ca7275f1ba25706ea7fc9f47a6d374fd
Ben <bjohnshawl@gmail.com>
2022-06-02 15:59:11 +0100

    Sum now working, tested, automated tests written for basic functions

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ from dataclasses import asdict
 import requests
 
 sys.path.append('Classes')
-from itertools import chain
 from discord.ext import commands
 from dotenv import load_dotenv
-from Classes.roll import Roll
+from src.functions.roll_functions import determine_if_sum, build_dice_list, handle_args, respond_to_roll
 
 
 load_dotenv()
@@ -22,8 +21,8 @@ INTENTS = discord.Intents.default()
 my_secret = os.getenv("TOKEN")
 
 client = commands.Bot(command_prefix='!',description='Now with more moreness!', case_insensitive=True, intents=INTENTS)
-#base_url = 'http://localhost:7071/api/roll'
-base_url = os.getenv("BASE_URL")
+base_url = 'http://localhost:7071/api/roll'
+#base_url = os.getenv("BASE_URL")
 
 @client.event
 async def on_ready():
@@ -32,88 +31,32 @@ async def on_ready():
 
 @client.command()
 async def roll(ctx,*args):
-  dice = [] # this becomes a list of tuples with the batch pairings
-  bonuses = []
-  arguments = [*args]
-  is_sum = determine_if_sum(arguments) # this can stay - we still want the sum functionality in, but we're going to simply return the Sum attr of the response from the api
-
-  
-  def handle_args(args):
-    for i, v in enumerate(args):
-        if str(v[0]) not in ['+', '-']:
-            dice.append([v])
-        else:
-            try:
-                if len(dice[-1]) > 1:
-                    raise
-                dice[-1].append(v)
-            except:
-                print("You have a modifier without a roll")
-
-  handle_args(arguments)
-
-  message_body={"dice":[]}
-
-  def build_dice_list(batch):
-    pattern = re.compile('([-|+])')
-    mod, neg = None, None
-    quantity, sides =[int(x) for x in str.upper(batch[0]).split('D')]
-    
-    if len(batch)>1:
-      _, sign, mod = re.split(pattern, batch[1])
-      neg = True if sign == '-' else False 
-    
-    roll = Roll(
-    quantity,sides,int(mod),neg
-    ) if not neg is None else Roll(quantity,sides)
-    b = asdict(roll)
-    message_body['dice'].append(b)
-
   username = ctx.author.display_name
   results_embed = discord.Embed()
   results_embed.set_author(name=f"-- {username} has rolled the dice! --")
-
+  message_body={"dice":[]}
+  arguments = [*args]
+  is_valid_roll = False
+  
+  is_sum = determine_if_sum(arguments) # this can stay - we still want the sum functionality in, but we're going to simply return the Sum attr of the response from the api
+  dice = handle_args(arguments)
+    
   try:
     for roll in dice:
-      build_dice_list(roll)
+      b = build_dice_list(roll, is_sum)
+      message_body['dice'].append(b)
   except (ValueError, TypeError):
     results_embed.add_field(name="Buuuuuut...", value="something went wrong - please check what you're rolling")
-    await ctx.send(embed=results_embed)
-  response = requests.post(url=base_url,json=message_body).json()
-
+  else:
+    is_valid_roll = True
   
-
-  val=[]
-
-
-
-  for i,v in enumerate(response):
-    val = ''
-    is_modified = determine_if_modified(v)
-    if is_sum:
-      val = f"Total = {v['sum']} "
-    else:
-      totals = v["results"]
-      val = f" Result = {v['sum']}" if len(v['detail']) == 1 else f"{' , '.join([str(t) for t in totals])}"
-      if is_modified:
-        val += f" : ({', '.join([d for d in v['detail']])})"
-
-    results_embed.add_field(name=f"Roll #{i+1}",value=val, inline=False)
-    
+  if is_valid_roll:
+    response = requests.post(url=base_url,json=message_body).json()
+    for i,v in enumerate(response):
+      val =  respond_to_roll(v, is_sum)
+      results_embed.add_field(name=f"Roll #{i+1}",value=val, inline=False)
+  
   await ctx.send(embed=results_embed)
 
-
-
-def determine_if_sum(arg_list):
-  p = re.compile(r'[()]')
-  for i in arg_list:
-    if p.match(i):
-      return True
-    else:
-      return False
-def determine_if_modified(res):
-  pattern = re.compile('([-|+])')
-  details = "".join([d for d in res['detail']])
-  return bool(re.search(pattern=pattern, string=details))
 
 client.run(my_secret)
